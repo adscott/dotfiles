845a12171e7d1ef50cb8bce9e1fa84b5449874e0
Adam Scott <me@adams.co.tt>
2018-07-21 15:28:38 +1000

    Added bins setup.

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ set -x
 ln -sf `pwd`/files/emacs ~/.emacs
 ln -sf `pwd`/files/tmux.conf ~/.tmux.conf
 ln -sf `pwd`/files/bash_profile ~/.bash_profile
+
+mkdir -p ~/Tools/Path
+ln -s `pwd`/bin/dev ~/Tools/Path/dev
