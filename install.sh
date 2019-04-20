4187490f0d027b280063e2d2e3e814a5084135ac
vagrant <vagrant@localhost.localdomain>
2019-04-20 07:40:31 +0000

    Added first cut of vim config.

--- install.sh
+++ install.sh
@@ -6,6 +6,13 @@ set -x
 ln -sf `pwd`/files/emacs ~/.emacs
 ln -sf `pwd`/files/tmux.conf ~/.tmux.conf
 ln -sf `pwd`/files/bash_profile ~/.bash_profile
+ln -sf `pwd`/files/vimrc ~/.vimrc
 
 mkdir -p ~/Tools/Path
 ln -sf `pwd`/bin/dev ~/Tools/Path/dev
+
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+vim +PluginInstall +qall
