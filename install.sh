21810a8723e13b7dc1d9f888a8512719de59f920
Adam Scott <me@adams.co.tt>
2019-05-02 14:25:17 +1000

    Stuff.

--- install.sh
+++ install.sh
@@ -20,5 +20,3 @@ vim +PluginInstall +qall
 if [ ! -d ~/.tmux/plugins/tpm ]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
-
-~/.tmux/plugins/tpm/scripts/install_plugins.sh
