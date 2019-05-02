a912f2fd434b5212af55955b6445834188c8bfca
Adam Scott <me@adams.co.tt>
2019-05-02 11:41:26 +1000

    Adding tmux plugins

--- install.sh
+++ install.sh
@@ -16,3 +16,9 @@ if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
 fi
 
 vim +PluginInstall +qall
+
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
